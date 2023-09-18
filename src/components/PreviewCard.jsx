93ea0a4fe5b52d0b5388ef7da87df920a169850c
Abdelrahman Hany <98924552+AbdelrhmanAbuelnile@users.noreply.github.com>
2023-09-18 13:14:31 +0300

    here ya mazen

--- src/components/PreviewCard.jsx
+++ src/components/PreviewCard.jsx
@@ -1,21 +1,23 @@
-import { useEffect, useState } from "react"
-import{motion} from 'framer-motion'
-function PreviewCard({image,name,size,price,stock}){
-    return(
-        <div className={`card`}>
-            <h1 style={{fontSize:size}}>{name}</h1>
-            <img src={image} alt="" />
-            <p className="backname">PC</p>
-            <div className="price-add">
-                <div className="price">
-                    <span>price</span>
-                    <p>{price}$</p>
-                </div>
-                <button>+</button>
-            </div>
-            <div style={{position:'absolute',bottom:"2%",left:'10%'}}>{stock?'in stock':'out of stock'}</div>
+import { useEffect, useState } from "react";
+import { motion } from "framer-motion";
+function PreviewCard({ image, name, size, price, stock }) {
+  return (
+    <div className={`card`}>
+      <h1 style={{ fontSize: size }}>{name}</h1>
+      <img src={image} alt="" />
+      <p className="backname">PC</p>
+      <div className="price-add">
+        <div className="price">
+          <span>price</span>
+          <p>{price}$</p>
         </div>
-    )
+        <button>+</button>
+      </div>
+      {/* <div style={{ position: "absolute", bottom: "2%", left: "10%" }}>
+        {stock ? "in stock" : "out of stock"}
+      </div> */}
+    </div>
+  );
 }
 
-export default PreviewCard
+export default PreviewCard;
