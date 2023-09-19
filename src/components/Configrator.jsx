d28d828a2717e87e60f410c5db9b3beedd715580
SinSintro <sinsintro@gmail.com>
2023-09-19 06:48:23 +0300

    Ahmed_modification_needed

--- src/components/Configrator.jsx
+++ src/components/Configrator.jsx
@@ -1,26 +1,28 @@
 import React, { useEffect, useState } from "react";
-import { useLocation } from "react-router-dom";
 import { useSnapshot } from "valtio";
 import { Link } from "react-router-dom";
 import { state } from "../store";
 import { motion } from "framer-motion";
+import { useLocation } from "react-router-dom";
 
-
-const transition = { type: 'spring', duration: 1.5 }
+const transition = { type: "spring", duration: 1.5 };
 const config = {
   initial: { x: -100, opacity: 0, transition: { ...transition, delay: 0.2 } },
   animate: { x: 0, opacity: 1, transition: { ...transition, delay: 0 } },
-  exit: { x: -100, opacity: 0, transition: { ...transition, delay: 0 } }
-}
+  exit: { x: -100, opacity: 0, transition: { ...transition, delay: 0 } },
+};
 function Configrator() {
-    let location = useLocation()
-    useEffect(()=>{
-        if(location.pathname === '/config')
-        {
-        state.inConfigMode = true
-        state.intro = false
-      }
-    },[])
+  const location = useLocation();
+  const price = location.state?.price || 'N/A';
+  const name = location.state?.name || 'N/A';
+  console.log(location)
+
+  useEffect(() => {
+    if (location.pathname === "/config") {
+      state.inConfigMode = true;
+      state.intro = false;
+    }
+  }, []);
   const [selectedSquare, setSelectedSquare] = useState(null);
 
   const [selectedComponent, setSelectedComponent] = useState(null);
@@ -54,132 +56,130 @@ function Configrator() {
 
   return (
     // <div className="grey-container">
-      <motion.div {...config} className="blueish-container">
-        <div className="centered-text">
-          <h1 className="text-content">PC Builder</h1>
-        </div>
-        <div className="white-container"></div>
-        <div className="rightSection-container">
-          <div className="vertical-container">
-          <Link style={{textDecoration:'none'}} to={'/'}>
-              <div className="back-button" onClick={handleBackClick}>
-                <div className="back-icon">←</div>
-                <div className="back-text">Back</div>
-              </div>
+    <motion.div {...config} className="blueish-container">
+      <div className="centered-text">
+        <h1 className="text-content">PC Builder</h1>
+      </div>
+      <div className="white-container"></div>
+      <div className="rightSection-container">
+        <div className="vertical-container">
+          <Link style={{ textDecoration: "none" }} to={"/"}>
+            <div className="back-button" onClick={handleBackClick}>
+              <div className="back-icon">←</div>
+              <div className="back-text">Back</div>
+            </div>
           </Link>
-              
-            
 
-            <p className="config-text">Configure the PC</p>
+          <p className="config-text">Configure the PC</p>
 
-            <div className="selectable-squares">
-              <div
-                className={`square ${
-                  selectedSquare === "square1" ? "selected" : ""
-                }`}
-                onClick={() => handleSquareClick("square1")}
-              >
-                <div className="icon pc-case">CPU</div>
-              </div>
-              <div
-                className={`square ${
-                  selectedSquare === "square2" ? "selected" : ""
-                }`}
-                onClick={() => handleSquareClick("square2")}
-              >
-                <div className="icon pc-case">GPU</div>
-              </div>
-              <div
-                className={`square ${
-                  selectedSquare === "square3" ? "selected" : ""
-                }`}
-                onClick={() => handleSquareClick("square3")}
-              >
-                <div className="icon pc-case">RAM</div>
-              </div>
-              <div
-                className={`square ${
-                  selectedSquare === "square4" ? "selected" : ""
-                }`}
-                onClick={() => handleSquareClick("square4")}
-              >
-                <div className="icon pc-case">RGB</div>
-              </div>
+          <div className="selectable-squares">
+            <div
+              className={`square ${
+                selectedSquare === "square1" ? "selected" : ""
+              }`}
+              onClick={() => handleSquareClick("square1")}
+            >
+              <div className="icon pc-case">CPU</div>
             </div>
+            <div
+              className={`square ${
+                selectedSquare === "square2" ? "selected" : ""
+              }`}
+              onClick={() => handleSquareClick("square2")}
+            >
+              <div className="icon pc-case">GPU</div>
+            </div>
+            <div
+              className={`square ${
+                selectedSquare === "square3" ? "selected" : ""
+              }`}
+              onClick={() => handleSquareClick("square3")}
+            >
+              <div className="icon pc-case">RAM</div>
+            </div>
+            <div
+              className={`square ${
+                selectedSquare === "square4" ? "selected" : ""
+              }`}
+              onClick={() => handleSquareClick("square4")}
+            >
+              <div className="icon pc-case">RGB</div>
+            </div>
+          </div>
 
-            <p className="component-text">Component</p>
+          <p className="component-text">{name}</p>
 
-            <div className="selectable-component">
-              <div
-                className={`component ${
-                  selectedComponent === "c1" ? "selected" : ""
-                }`}
-                onClick={() => handleComponenetClick("c1")}
-              >
-                <div className="icon pc-case">CPU</div>
-              </div>
-              <div
-                className={`component ${
-                  selectedComponent === "c2" ? "selected" : ""
-                }`}
-                onClick={() => handleComponenetClick("c2")}
-              >
-                <div className="icon pc-case">GPU</div>
-              </div>
-              <div
-                className={`component ${
-                  selectedComponent === "c3" ? "selected" : ""
-                }`}
-                onClick={() => handleComponenetClick("c3")}
-              >
-                <div className="icon pc-case">RAM</div>
-              </div>
-              <div
-                className={`component ${
-                  selectedComponent === "c4" ? "selected" : ""
-                }`}
-                onClick={() => handleComponenetClick("c4")}
-              >
-                <div className="icon pc-case">RGB</div>
-              </div>
+          <div className="selectable-component">
+            <div
+              className={`component ${
+                selectedComponent === "c1" ? "selected" : ""
+              }`}
+              onClick={() => handleComponenetClick("c1")}
+            >
+              <div className="icon pc-case">CPU</div>
+            </div>
+            <div
+              className={`component ${
+                selectedComponent === "c2" ? "selected" : ""
+              }`}
+              onClick={() => handleComponenetClick("c2")}
+            >
+              <div className="icon pc-case">GPU</div>
+            </div>
+            <div
+              className={`component ${
+                selectedComponent === "c3" ? "selected" : ""
+              }`}
+              onClick={() => handleComponenetClick("c3")}
+            >
+              <div className="icon pc-case">RAM</div>
+            </div>
+            <div
+              className={`component ${
+                selectedComponent === "c4" ? "selected" : ""
+              }`}
+              onClick={() => handleComponenetClick("c4")}
+            >
+              <div className="icon pc-case">RGB</div>
             </div>
           </div>
         </div>
+      </div>
 
-        <div className="leftSection-container">
-          <div className="vertical-container">
-            <div
-              className={`leftsquare ${
-                selectedleftSquare === "leftsquare1" ? "selected" : ""
-              }`}
-              onClick={() => handleleftSquareClick("leftsquare1")}
-            >
-              <div className="icon pc-case"></div>
-            </div>
-            <div
-              className={`leftsquare ${
-                selectedleftSquare === "leftsquare2" ? "selected" : ""
-              }`}
-              onClick={() => handleleftSquareClick("leftsquare2")}
-            >
-              <div className="icon pc-case"></div>
-            </div>
-          </div>
-        </div>
-
-        <div className="price-container">
-          <div>
-            <div className="price-text">Price</div>
-            <div className="price-value">$250</div>
-          </div>
-          <button
-            className={`add-to-cart-button ${isButtonClicked ? "clicked" : ""}`}
-            onClick={handleButtonClick}
+      <div className="leftSection-container">
+        <div className="vertical-container">
+          <div
+            className={`leftsquare ${
+              selectedleftSquare === "leftsquare1" ? "selected" : ""
+            }`}
+            onClick={() => handleleftSquareClick("leftsquare1")}
           >
-            + Add to Cart
-          </button>
+            <div className="icon pc-case"></div>
+          </div>
+          <div
+            className={`leftsquare ${
+              selectedleftSquare === "leftsquare2" ? "selected" : ""
+            }`}
+            onClick={() => handleleftSquareClick("leftsquare2")}
+          >
+            <div className="icon pc-case"></div>
+          </div>
         </div>
-      </motion.div>
+      </div>
+
+      <div className="price-container">
+        <div>
+          <div className="price-text">Price</div>
+          <div className="price-value">${price}</div>
+        </div>
+        <button
+          className={`add-to-cart-button ${isButtonClicked ? "clicked" : ""}`}
+          onClick={handleButtonClick}
+        >
+          + Add to Cart
+        </button>
+      </div>
+    </motion.div>
     // </div>
   );
 }
--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,70 +1,249 @@
-import {Swiper,SwiperSlide } from "swiper/react"
-import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
-import { BrowserView, MobileView, isBrowser, isMobile } from 'react-device-detect';
-import 'swiper/css';
-import {Link} from 'react-router-dom'
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
-import 'swiper/css/scrollbar';
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination, Scrollbar, A11y } from "swiper/modules";
+import {
+  BrowserView,
+  MobileView,
+  isBrowser,
+  isMobile,
+} from "react-device-detect";
+import "swiper/css";
+import { Link } from "react-router-dom";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
 // import { motion } from "framer-motion";
-import pc1 from '../assets/cases/case1.png'
-import pc2 from '../assets/cases/case2.png'
-import pc3 from '../assets/cases/case3.png'
-import pc4 from '../assets/cases/case4.png'
-import pc5 from '../assets/cases/case5.png'
-import pc6 from '../assets/cases/case6.png'
-import pc7 from '../assets/cases/case7.png'
+import pc1 from "../assets/cases/case1.png";
+import pc2 from "../assets/cases/case2.png";
+import pc3 from "../assets/cases/case3.png";
+import pc4 from "../assets/cases/case4.png";
+import pc5 from "../assets/cases/case5.png";
+import pc6 from "../assets/cases/case6.png";
+import pc7 from "../assets/cases/case7.png";
 
-import PreviewCard from "../components/PreviewCard"
+import PreviewCard from "../components/PreviewCard";
 import { motion } from "framer-motion";
 import { useEffect, useState } from "react";
-const transition = { type: 'spring', duration: 2 ,delay:0.1}
-function Shop(){
-
-  const [mobile,setMobile] = useState(isMobile)
-  useEffect(()=>{
-    document.addEventListener('resize',()=>{
-      setMobile(isMobile)
-    })
-    
-  },)
-    return(
-        <motion.div 
-        initial={{ opacity: 0, x: -100 }} animate={{ opacity: 1, x: 0 ,zIndex:2}} transition={transition}
-        className="card-wrapper" >
-        <Swiper
+const transition = { type: "spring", duration: 2, delay: 0.1 };
+function Shop() {
+  const [mobile, setMobile] = useState(isMobile);
+  useEffect(() => {
+    document.addEventListener("resize", () => {
+      setMobile(isMobile);
+    });
+  });
+  console.log("State to be sent:", {
+    price: 80,
+    name: "MSI MAG VAMPIRIC 100R Midi Tower Case",
+  });
+  return (
+    <motion.div
+      initial={{ opacity: 0, x: -100 }}
+      animate={{ opacity: 1, x: 0, zIndex: 2 }}
+      transition={transition}
+      className="card-wrapper"
+    >
+      <Swiper
         modules={[Scrollbar]}
         spaceBetween={100}
-        slidesPerView={mobile?1:4}
-        loop={true}
+        slidesPerView={mobile ? 1 : 4}
+        loop={true} 
         speed={600}
-        >
+      >
         <SwiperSlide>
-
-          <PreviewCard
-            stock={false}
-            price={80}
-            size={8}
-            name={'MSI MAG VAMPIRIC 100R Midi Tower Case'}
-            image={pc1}
-          />
-
+          <Link
+            to={{
+              pathname: "/config",
+              state: {
+                price: 80,
+                name: "MSI MAG VAMPIRIC 100R Midi Tower Case",
+              },
+            }}
+          >
+            <PreviewCard
+              stock={false}
+              price={80}
+              size={8}
+              name={"MSI MAG VAMPIRIC 100R Midi Tower Case"}
+              image={pc1}
+            />
+          </Link>
         </SwiperSlide>
 
-        <SwiperSlide><PreviewCard stock price={150} size={7} name={'NZXT H5 Flow RGB Black Midi Tower Behuizing'} image={pc2}/></SwiperSlide>
-        <SwiperSlide><PreviewCard stock price={150} size={6.5} name={'Fractal Design Pop Air RGB Black TG Clear Tint Midi Tower'} image={pc3}/></SwiperSlide>
-        <SwiperSlide><PreviewCard stock price={200} size={12} name={'Antec NX 410 Midi Tower'} image={pc4}/></SwiperSlide>
-        <SwiperSlide><PreviewCard stock price={120} size={10} name={'Phanteks Eclipse G300A Black 3-fan Midi Tower Behuizing'} image={pc5}/></SwiperSlide>
-        <SwiperSlide><PreviewCard stock price={100} size={9} name={'DeepCool CC560 A-RGB Midi Tower'} image={pc6}/></SwiperSlide>
-        <SwiperSlide><PreviewCard stock price={300} size={6} name={'be quiet! Pure Base 500DX Black Midi Tower'} image={pc7}/></SwiperSlide>
-        <SwiperSlide><PreviewCard stock price={80} size={8} name={'MSI MAG VAMPIRIC 100R Midi Tower Case'} image={pc1}/></SwiperSlide>
-        <SwiperSlide><PreviewCard stock price={150} size={7} name={'NZXT H5 Flow RGB Black Midi Tower Behuizing'} image={pc2}/></SwiperSlide>
-        <SwiperSlide><PreviewCard stock price={150} size={6.5} name={'Fractal Design Pop Air RGB Black TG Clear Tint Midi Tower'} image={pc3}/></SwiperSlide>
-        </Swiper>
-
-        
-      </motion.div>
-    )
+        <SwiperSlide>
+        <Link
+            to={{
+              pathname: "/config",
+              state: {
+                price: 150,
+                name: "NZXT H5 Flow RGB Black Midi Tower Behuizing",
+              },
+            }}
+          >
+          <PreviewCard
+            stock
+            price={150}
+            size={7}
+            name={"NZXT H5 Flow RGB Black Midi Tower Behuizing"}
+            image={pc2}
+          />
+          </Link>
+        </SwiperSlide>
+        <SwiperSlide>
+        <Link
+            to={{
+              pathname: "/config",
+              state: {
+                price: 150,
+                name: "Fractal Design Pop Air RGB Black TG Clear Tint Midi Tower",
+              },
+            }}
+          >
+          <PreviewCard
+            stock
+            price={150}
+            size={6.5}
+            name={"Fractal Design Pop Air RGB Black TG Clear Tint Midi Tower"}
+            image={pc3}
+          />
+          </Link>
+        </SwiperSlide>
+        <SwiperSlide>
+        <Link
+            to={{
+              pathname: "/config",
+              state: {
+                price: 200,
+                name: "Antec NX 410 Midi Tower",
+              },
+            }}
+          >
+          <PreviewCard
+            stock
+            price={200}
+            size={12}
+            name={"Antec NX 410 Midi Tower"}
+            image={pc4}
+          />
+          </Link>
+        </SwiperSlide>
+        <SwiperSlide>
+        <Link
+            to={{
+              pathname: "/config",
+              state: {
+                price: 120,
+                name: "Phanteks Eclipse G300A Black 3-fan Midi Tower Behuizing",
+              },
+            }}
+          >
+          <PreviewCard
+            stock
+            price={120}
+            size={10}
+            name={"Phanteks Eclipse G300A Black 3-fan Midi Tower Behuizing"}
+            image={pc5}
+          />
+          </Link>
+        </SwiperSlide>
+        <SwiperSlide>
+        <Link
+            to={{
+              pathname: "/config",
+              state: {
+                price: 100,
+                name: "DeepCool CC560 A-RGB Midi Tower",
+              },
+            }}
+          >
+          <PreviewCard
+            stock
+            price={100}
+            size={9}
+            name={"DeepCool CC560 A-RGB Midi Tower"}
+            image={pc6}
+          />
+          </Link>
+        </SwiperSlide>
+        <SwiperSlide>
+        <Link
+            to={{
+              pathname: "/config",
+              state: {
+                price: 300,
+                name: "be quiet! Pure Base 500DX Black Midi Tower",
+              },
+            }}
+          >
+          <PreviewCard
+            stock
+            price={300}
+            size={6}
+            name={"be quiet! Pure Base 500DX Black Midi Tower"}
+            image={pc7}
+          />
+          </Link>
+        </SwiperSlide>
+        <SwiperSlide>
+        <Link
+            to={{
+              pathname: "/config",
+              state: {
+                price: 80,
+                name: "MSI MAG VAMPIRIC 100R Midi Tower Case",
+              },
+            }}
+          >
+          <PreviewCard
+            stock
+            price={80}
+            size={8}
+            name={"MSI MAG VAMPIRIC 100R Midi Tower Case"}
+            image={pc1}
+          />
+          </Link>
+        </SwiperSlide>
+        <SwiperSlide>
+        <Link
+            to={{
+              pathname: "/config",
+              state: {
+                price: 150,
+                name: "NZXT H5 Flow RGB Black Midi Tower Behuizing",
+              },
+            }}
+          >
+          <PreviewCard
+            stock
+            price={150}
+            size={7}
+            name={"NZXT H5 Flow RGB Black Midi Tower Behuizing"}
+            image={pc2}
+          />
+          </Link>
+        </SwiperSlide>
+        <SwiperSlide>
+        <Link
+            to={{
+              pathname: "/config",
+              state: {
+                price: 150,
+                name: "Fractal Design Pop Air RGB Black TG Clear Tint Midi Tower",
+              },
+            }}
+          >
+          <PreviewCard
+            stock
+            price={150}
+            size={6.5}
+            name={"Fractal Design Pop Air RGB Black TG Clear Tint Midi Tower"}
+            image={pc3}
+          />
+          </Link>
+        </SwiperSlide>
+      </Swiper>
+    </motion.div>
+  );
+  
 }
 
-export default Shop
+export default Shop;
