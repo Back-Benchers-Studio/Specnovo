e010783b0e9b5b5b58ba219982efae80aae10559
Abdelrahman Hany <98924552+AbdelrhmanAbuelnile@users.noreply.github.com>
2023-10-25 00:01:02 +0300

    mangaaaa

--- src/components/PreviewCard.jsx
+++ src/components/PreviewCard.jsx
@@ -1,21 +1,31 @@
 import { useEffect, useState } from "react";
 import { motion } from "framer-motion";
-function PreviewCard({ image, name, size, price, stock }) {
+import { BrowserView, MobileView, isBrowser, isMobile } from 'react-device-detect';
+import {AiOutlinePlus,AiOutlineHeart,AiFillHeart} from "react-icons/ai"
+
+function PreviewCard({ image, name, size, price, stock, fav }) {
+
+  const [mobile,setMobile] = useState(isMobile)
+  useEffect(()=>{
+    window.addEventListener('resize',()=>{
+      setMobile(isMobile)
+    })
+    
+  },)
   return (
     <div className={`card`}>
-      <h1 style={{ fontSize: size }}>{name}</h1>
       <img src={image} alt="" />
-      <p className="backname">PC</p>
+        {/* <p>. . .</p> */}
       <div className="price-add">
         <div className="price">
-          <span>price</span>
+          <h1>{name}</h1>
           <p>{price}$</p>
         </div>
-        <button>+</button>
+        <div className="controler-btns">
+          <button><AiOutlinePlus/></button>
+          <button style={{color: fav? "#ff534d" : ""}}>{fav?<AiFillHeart/> : <AiOutlineHeart/>}</button>
+        </div>
       </div>
-      {/* <div style={{ position: "absolute", bottom: "2%", left: "10%" }}>
-        {stock ? "in stock" : "out of stock"}
-      </div> */}
     </div>
   );
 }
--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,6 +1,6 @@
 import {Swiper,SwiperSlide } from "swiper/react"
 import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
-import { BrowserView, MobileView, isBrowser, isMobile } from 'react-device-detect';
+import { BrowserView, MobileView, isTablet, isMobile } from 'react-device-detect';
 import 'swiper/css';
 import {Link} from 'react-router-dom'
 import 'swiper/css/navigation';
@@ -21,23 +21,64 @@ import { useEffect, useState } from "react";
 const transition = { type: 'spring', duration: 2 ,delay:0.1}
 function Shop(){
 
-  const [mobile,setMobile] = useState(isMobile)
-  useEffect(()=>{
-    document.addEventListener('resize',()=>{
-      setMobile(isMobile)
-    })
-    
-  },)
+  const [deviceType, setDeviceType] = useState(
+    isMobile ? 'Mobile' : isTablet ? 'Tablet' : 'Desktop'
+  );
+
+  useEffect(() => {
+    const handleResize = () => {
+      const newDeviceType = isMobile ? 'Mobile' : isTablet ? 'Tablet' : 'Desktop';
+      setDeviceType(newDeviceType);
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    // Clean up the event listener when the component unmounts
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  const swiperConfig = {
+    Mobile: {
+      spaceBetween: 150,
+      slidesPerView: 1,
+    },
+    Tablet: {
+      spaceBetween: 120,
+      slidesPerView: 2,
+    },
+    Desktop: {
+      spaceBetween: 100,
+      slidesPerView: 4,
+    },
+  };
+
+  const { spaceBetween, slidesPerView } = swiperConfig[deviceType];
+
     return(
         <motion.div 
         initial={{ opacity: 0, x: -100 }} animate={{ opacity: 1, x: 0 ,zIndex:2}} transition={transition}
         className="card-wrapper" >
+          <span id="bolb1" className="bolb"></span>
+          <span id="bolb2" className="bolb"></span>
         <Swiper
         modules={[Scrollbar]}
-        spaceBetween={100}
-        slidesPerView={mobile?1:4}
+        spaceBetween={spaceBetween}
+        slidesPerView={slidesPerView}
         loop={true}
         speed={600}
+        breakpoints={{
+          1024: {
+            spaceBetween: 150, // Adjust these values as needed
+            slidesPerView: 3,   // Adjust these values as needed
+          },
+          800:{
+            spaceBetween: 50,
+            slidesPerView: 2,
+          }
+
+        }}
         >
         <SwiperSlide>
 
@@ -51,15 +92,15 @@ function Shop(){
 
         </SwiperSlide>
 
-        <SwiperSlide><PreviewCard stock price={150} size={7} name={'NZXT H5 Flow RGB Black Midi Tower Behuizing'} image={pc2}/></SwiperSlide>
-        <SwiperSlide><PreviewCard stock price={150} size={6.5} name={'Fractal Design Pop Air RGB Black TG Clear Tint Midi Tower'} image={pc3}/></SwiperSlide>
-        <SwiperSlide><PreviewCard stock price={200} size={12} name={'Antec NX 410 Midi Tower'} image={pc4}/></SwiperSlide>
-        <SwiperSlide><PreviewCard stock price={120} size={10} name={'Phanteks Eclipse G300A Black 3-fan Midi Tower Behuizing'} image={pc5}/></SwiperSlide>
-        <SwiperSlide><PreviewCard stock price={100} size={9} name={'DeepCool CC560 A-RGB Midi Tower'} image={pc6}/></SwiperSlide>
-        <SwiperSlide><PreviewCard stock price={300} size={6} name={'be quiet! Pure Base 500DX Black Midi Tower'} image={pc7}/></SwiperSlide>
-        <SwiperSlide><PreviewCard stock price={80} size={8} name={'MSI MAG VAMPIRIC 100R Midi Tower Case'} image={pc1}/></SwiperSlide>
-        <SwiperSlide><PreviewCard stock price={150} size={7} name={'NZXT H5 Flow RGB Black Midi Tower Behuizing'} image={pc2}/></SwiperSlide>
-        <SwiperSlide><PreviewCard stock price={150} size={6.5} name={'Fractal Design Pop Air RGB Black TG Clear Tint Midi Tower'} image={pc3}/></SwiperSlide>
+        <SwiperSlide><PreviewCard stock fav={true} price={150} size={7} name={'NZXT H5 Flow RGB Black Midi Tower Behuizing'} image={pc2}/></SwiperSlide>
+        <SwiperSlide><PreviewCard stock fav={false} price={150} size={7} name={'Fractal Design Pop Air RGB Black TG Clear Tint Midi Tower'} image={pc3}/></SwiperSlide>
+        <SwiperSlide><PreviewCard stock fav={true} price={200} size={12} name={'Antec NX 410 Midi Tower'} image={pc4}/></SwiperSlide>
+        <SwiperSlide><PreviewCard stock fav={true} price={120} size={10} name={'Phanteks Eclipse G300A Black 3-fan Midi Tower Behuizing'} image={pc5}/></SwiperSlide>
+        <SwiperSlide><PreviewCard stock fav={false} price={100} size={9} name={'DeepCool CC560 A-RGB Midi Tower'} image={pc6}/></SwiperSlide>
+        <SwiperSlide><PreviewCard stock fav={false} price={300} size={6} name={'be quiet! Pure Base 500DX Black Midi Tower'} image={pc7}/></SwiperSlide>
+        <SwiperSlide><PreviewCard stock fav={true} price={80} size={8} name={'MSI MAG VAMPIRIC 100R Midi Tower Case'} image={pc1}/></SwiperSlide>
+        <SwiperSlide><PreviewCard stock fav={false} price={150} size={7} name={'NZXT H5 Flow RGB Black Midi Tower Behuizing'} image={pc2}/></SwiperSlide>
+        <SwiperSlide><PreviewCard stock fav={true} price={150} size={7} name={'Fractal Design Pop Air RGB Black TG Clear Tint Midi Tower'} image={pc3}/></SwiperSlide>
         </Swiper>
 
         
